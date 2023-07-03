206dd3616f860484ebb9716abf4754e3e91b23a0
sckakarla225 <sck225@cornell.edu>
2023-07-03 12:01:24 -0400

    setup more config stuff

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,18 @@
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="container">
+  <button class="button">Click me</button>
+</div>
+
+<h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
+
+<style lang="postcss">
+  /* :global(html) {
+    background-color: theme(colors.gray.100);
+  } */
+</style>
